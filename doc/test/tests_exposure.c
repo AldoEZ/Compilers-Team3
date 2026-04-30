77a640d128d85a7d436511609fbe465c8a5780f9
Aldo EZ <zacariasaldo.19@gmail.com>
2026-04-30 02:16:59 -0600

    tests for exposure

---
+++ doc/test/tests_exposure.c
@@ -0,0 +1,69 @@
+// test 1
+int main() {
+    return 0;
+}
+
+// test2
+/*
+hi everyone
+*/
+int main() {
+    int x = 5;
+    
+    printf("x=%d", x);
+    return 0;
+}
+
+// test 3
+int main() {
+    int x = 5;
+    int x = 8;
+    
+    return 0;
+}
+
+// test 4
+int main() {
+    int x = 5;
+    int y = 2;
+    x = x + y * 3;
+    if (x > 0) {
+        x--;
+    } else {
+        x = 0;
+    }
+    printf("x=%d", x);
+    return x;
+}
+
+// test 6
+int main() {
+    int x = 5;
+    int y = 2;
+    hola();
+    
+    return x;
+}
+
+void hola() {
+    printf("Hiiii");
+}
+
+// test 7
+int main() {
+    int x = "I know nothing";
+    
+    return x;
+}
+
+// test 8
+int hi() {
+    return 0;
+}
+
+// test 9
+int main() {
+    x = "I know nothing";
+    
+    return 0;
+}
